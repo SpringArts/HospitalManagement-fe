8b61489ef7fc1214202609172b3b80b45a829cf9
aungzawphyo <aungzawphyo1102@gmail.com>
2023-09-22 21:31:23 +0630

    raw layout

--- src/components/doctor/Layout.jsx
+++ src/components/doctor/Layout.jsx
@@ -6,7 +6,7 @@ import {AiOutlineMenu} from 'react-icons/ai'
 const Layout = (props) => {
     return  (
         <div className="flex">
-            <div className="w-1/5 p-3 bg-[#eee] text-[#333] min-h-screen">
+            <div className="w-1/5 p-3 shadow text-[#333] min-h-screen">
                 <div className="mb-3">
                     <Image className="rounded-full border-[#fff] border-2 mx-auto" alt='Doctor Profile' width={150} height={150} src='/doctor.webp'/>
                 </div>
@@ -33,8 +33,8 @@ const Layout = (props) => {
                     </li>
                 </ul>
             </div>
-            <div className="w-4/5 p-3">
-                <div className="mb-4 shadow-lg bg-[#EEEEEE] rounded p-3 flex justify-between items-center sticky top-0">
+            <div className="w-4/5 p-3 bg-[#ffc0cb93]">
+                <div className="mb-4 shadow bg-[#fff] rounded p-3 flex justify-between items-center sticky top-0">
                     <h1 className="text-[26px]">{props.title}</h1>
                     <AiOutlineMenu size={30}/>
                 </div>

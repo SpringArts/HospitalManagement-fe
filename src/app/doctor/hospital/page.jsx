e3d3415ef339d2e09ab6b8dd2399121bda9daf9c
aungzawphyo <aungzawphyo1102@gmail.com>
2023-09-30 16:34:31 +0630

    add pagination

--- src/app/doctor/hospital/page.jsx
+++ src/app/doctor/hospital/page.jsx
@@ -1,42 +1,46 @@
 "use client"
 
 import Layout from "@/components/doctor/Layout";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import {FaRegHospital, FaLocationArrow} from 'react-icons/fa'
 import {BsTelephone} from 'react-icons/bs'
 import {HiOutlineMail} from 'react-icons/hi'
 import Image from "next/image";
+import ReactPaginate from "react-paginate";
 
 const HospitalPage = (props) => {
-    const [hospitals, setHospitals] = useState([])
+    const [hospitalData, setHospitalData] = useState([])
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage, setItemsPerPage] = useState(8);
     const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const httpRequest = async () => {
-            const response = await fetch('http://localhost:8000/api/hospitals', {
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                }
-            })
-            if(!response.ok) {
-                setError('Something wrong.')
-                return
+    const httpRequest = useCallback(async () => {
+        const response = await fetch(`http://localhost:8000/api/hospitals?page=${currentPage}&perPage=${itemsPerPage}`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
             }
-            const responseData = await response.json()
-            console.log(responseData.data)
-            setHospitals(responseData.data.hospitals)
+        })
+        if(!response.ok) {
+            setError('Something wrong.')
+            return
         }
+        const responseData = await response.json()
+        console.log(responseData.data)
+        setHospitalData(responseData.data)
+    }, [currentPage, itemsPerPage])
+
+    useEffect(() => {
         httpRequest()
-    }, [])
+    }, [currentPage, httpRequest])
 
     return (
         <Layout title='Hospitals'>
             <div>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
                     {
-                        !error && hospitals.map(hospital => (
+                        !error && hospitalData.hospitals && hospitalData.hospitals.map(hospital => (
                         <a key={hospital.id}
                             href="#"
                             className="relative block overflow-hidden rounded shadow-lg border border-gray-100"
@@ -63,6 +67,25 @@ const HospitalPage = (props) => {
                         ))
                     }
                 </div>
+
+                <ReactPaginate
+                        breakLabel="..."
+                        nextLabe="next"
+                        onPageChange={(page) => {
+                            
+                            setCurrentPage(page?.selected + 1)
+                        }}
+                        pageRangeDisplayed={3}
+                        pageCount={hospitalData?.meta?.totalPages}
+                        prevPageRel="prev"
+                        renderOnZeroPageCount={null}
+                        breakClassName="border border-white hover:bg-pink-200 transition-all duration-200 ease-in-out disabled:opacity-70 disabled:text-gray-100 mb-10 px-4 py-2 text-sm rounded-full text-lg"
+                        activeClassName="bg-green-200 hover:bg-green-300 text-white transition-all duration-300 ease-in-out"
+                        previousClassName="border border-white hover:bg-pink-200 transition-all duration-200 ease-in-out disabled:opacity-70 disabled:text-gray-100 mb-10 px-8 py-3 text-sm rounded-full tracking tracking-wider"
+                        nextClassName="border border-white hover:bg-pink-200 transition-all duration-200 ease-in-out disabled:opacity-70 disabled:text-gray-100 mb-10 px-8 py-3 text-sm rounded-full tracking tracking-wider"
+                        containerClassName="flex justify-center items-center w-full mt-8 gap-x-3"
+                        pageClassName="border border-white hover:bg-pink-200 transition-all duration-200 ease-in-out disabled:opacity-70 disabled:text-gray-100 mb-10 px-4 py-3 text-sm rounded-full tracking tracking-wider"
+                    />
             </div>
         </Layout>
     );
--- src/app/doctor/patient/page.jsx
+++ src/app/doctor/patient/page.jsx
@@ -2,40 +2,45 @@
 
 import Layout from "@/components/doctor/Layout";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
 import { BsTelephone } from "react-icons/bs";
 import { AiOutlineUser } from "react-icons/ai";
 import { HiOutlineMail, HiOutlineLocationMarker } from "react-icons/hi";
 
 const PatientPage = () => {
-    const [patients, setPatients] = useState([])
+    const [patientData, setPatientData] = useState([])
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage, setItemsPerPage] = useState(8);
     const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const httpRequest = async () => {
-            const response = await fetch('http://localhost:8000/api/patients', {
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                }
-            })
-            if(!response.ok) {
-                setError('Something wrong.')
-                return
+    const httpRequest = useCallback(async () => {
+        const response = await fetch(`http://localhost:8000/api/patients?page=${currentPage}&perPage=${itemsPerPage}`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
             }
-            const responseData = await response.json()
-            console.log(responseData.data)
-            setPatients(responseData.data)
+        })
+        if(!response.ok) {
+            setError('Something wrong.')
+            return
         }
+        const responseData = await response.json()
+        console.log(responseData)
+        setPatientData(responseData)
+    }, [currentPage, itemsPerPage])
+
+    useEffect(() => {
         httpRequest()
-    }, [])
+    }, [currentPage, httpRequest])
+
   return (
     <Layout title='Patients'>
         <div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
                 {
-                    !error && patients.map(patient => (
+                    !error && patientData.data && patientData.data.map(patient => (
                         <a key={patient}
                             href="#"
                             className="relative block overflow-hidden rounded-lg shadow border border-gray-100"
@@ -66,6 +71,25 @@ const PatientPage = () => {
                     ))
                 }
             </div>
+
+            <ReactPaginate
+                        breakLabel="..."
+                        nextLabe="next"
+                        onPageChange={(page) => {
+                            
+                            setCurrentPage(page?.selected + 1)
+                        }}
+                        pageRangeDisplayed={3}
+                        pageCount={patientData?.meta?.totalPages}
+                        prevPageRel="prev"
+                        renderOnZeroPageCount={null}
+                        breakClassName="border border-white hover:bg-pink-200 transition-all duration-200 ease-in-out disabled:opacity-70 disabled:text-gray-100 mb-10 px-4 py-2 text-sm rounded-full text-lg"
+                        activeClassName="bg-green-200 hover:bg-green-300 text-white transition-all duration-300 ease-in-out"
+                        previousClassName="border border-white hover:bg-pink-200 transition-all duration-200 ease-in-out disabled:opacity-70 disabled:text-gray-100 mb-10 px-8 py-3 text-sm rounded-full tracking tracking-wider"
+                        nextClassName="border border-white hover:bg-pink-200 transition-all duration-200 ease-in-out disabled:opacity-70 disabled:text-gray-100 mb-10 px-8 py-3 text-sm rounded-full tracking tracking-wider"
+                        containerClassName="flex justify-center items-center w-full mt-8 gap-x-3"
+                        pageClassName="border border-white hover:bg-pink-200 transition-all duration-200 ease-in-out disabled:opacity-70 disabled:text-gray-100 mb-10 px-4 py-3 text-sm rounded-full tracking tracking-wider"
+                    />
         </div>
     </Layout>
 );

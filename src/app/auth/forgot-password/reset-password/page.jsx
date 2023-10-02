954412d8b59f1d9c2074f16d42f893bc42f3f81a
LwinMoeAung <53222794+lwinmoeaung-springarts@users.noreply.github.com>
2023-10-02 22:11:39 +0900

    fix ui design

--- src/app/auth/forgot-password/reset-password/page.jsx
+++ src/app/auth/forgot-password/reset-password/page.jsx
@@ -1,5 +1,5 @@
 'use client';
-import axios from "axios";
+import axios from "@/lib/axios";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
@@ -7,22 +7,22 @@ import toast from "react-hot-toast";
 
 const page = () => {
 
-  const router = useRouter();
-    const [code , setCode] = useState("");
-    const [password , setPassword] = useState("");
-    const [password_confirmation , setPasswordConfirmation] = useState("");
+    const router = useRouter();
+    const [code, setCode] = useState("");
+    const [password, setPassword] = useState("");
+    const [password_confirmation, setPasswordConfirmation] = useState("");
 
-    const handleSubmit = async(e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        try{
-            await axios.post(`http://127.0.0.1:8000/api/password/reset`, {
-                code , password , password_confirmation
-            }).then((response)=>{
+        try {
+            await axios.post(`/password/reset`, {
+                code, password, password_confirmation
+            }).then((response) => {
                 console.log(response)
                 toast.success(response.message)
                 router.push("/auth/login")
             })
-        }catch(error){
+        } catch (error) {
             if (error.response && error.response.status === 422) {
                 const validationErrors = error.response.data.errors;
                 toast.error("Validation errors:", validationErrors);
@@ -33,65 +33,65 @@ const page = () => {
     }
 
     return (
-        <div className="overflow-hidden">
-            <div className="min-h-screen flex items-center justify-center bg-gray-100">
-                <div className="min-w-full h-screen relative bg-gradient-to-br from-teal-300 to-blue-500 p-5">
-                    <div className="flex h-screen flex-col sm:flex-row items-center justify-center sm:pl-6 md:pl-24 ">
-                        <div className="w-full sm:w-96 rounded-lg shadow-lg p-6 sm:p-10 bg-[#C1F8FC]">
-                            <form
-                                className="flex flex-col w-full max-w-sm mx-auto px-4 py-8 bg-white shadow-md rounded-lg"
-                                onSubmit={handleSubmit}
-                            >
-                                <h1 className="text-2xl font-bold mb-4">
-                                    Reset Password
-                                </h1>
-                                <p className="text-gray-600 mb-4">
-                                    Enter your reset password code that we sent to your mail
-                                </p>
-                                <input
-                                    type="number"
-                                    value={code}
-                                    onChange={(e)=>setCode(e.target.value)}
-                                    className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-sky-500 mb-4"
-                                    placeholder="Reset Code"
-                                />
-                                <input
-                                    type="password"
-                                    value={password}
-                                    onChange={(e)=>setPassword(e.target.value)}
-                                    className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-sky-500 mb-4"
-                                    placeholder="Enter new password"
-                                />
-                                <input
-                                    type="password"
-                                    value={password_confirmation}
-                                    onChange={(e)=>setPasswordConfirmation(e.target.value)}
-                                    className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-sky-500 mb-4"
-                                    placeholder="Reenter your password"
-                                />
-                                <button
-                                    type="submit"
-                                    className="bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline"
-                                >
-                                    Send
-                                </button>
-                            </form>
-                        </div>
+        <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-teal-300 to-blue-500">
+            <div className="flex flex-col sm:flex-row items-center justify-center p-5 sm:p-10 bg-[#C1F8FC] rounded-lg shadow-lg">
+                <div className="w-full sm:w-96 p-8">
+                    <form
+                        className="flex flex-col max-w-sm mx-auto"
+                        onSubmit={handleSubmit}
+                    >
+                        <h1 className="text-3xl font-bold mb-4 text-gray-800 text-center">
+                            Reset Password
+                        </h1>
+                        <p className="text-gray-600 mb-6 text-center">
+                            Enter the reset password code that we sent to your email, along with your new password.
+                        </p>
+                        <input
+                            type="number"
+                            value={code}
+                            onChange={(e) => setCode(e.target.value)}
+                            className="px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:border-sky-500 mb-4"
+                            placeholder="Reset Code"
+                            required
+                        />
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:border-sky-500 mb-4"
+                            placeholder="Enter new password"
+                            required
+                        />
+                        <input
+                            type="password"
+                            value={password_confirmation}
+                            onChange={(e) => setPasswordConfirmation(e.target.value)}
+                            className="px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:border-sky-500 mb-4"
+                            placeholder="Reenter your password"
+                            required
+                        />
+                        <button
+                            type="submit"
+                            className="bg-sky-500 hover:bg-sky-700 text-white font-bold py-3 px-6 rounded-md focus:outline-none focus:shadow-outline"
+                        >
+                            Reset Password
+                        </button>
+                    </form>
+                </div>
 
-                        <div className="flex items-center p-3 mt-4 md:ml-48">
-                            <Image
-                                className="relative hidden md:block"
-                                src="/login.svg"
-                                alt="Register Logo"
-                                width={750}
-                                height={750}
-                                priority
-                            />
-                        </div>
-                    </div>
+                <div className="flex items-center p-3 mt-4 md:ml-8">
+                    <Image
+                        className="hidden md:block"
+                        src="/login.svg"
+                        alt="Register Logo"
+                        width={400}
+                        height={400}
+                        priority
+                    />
                 </div>
             </div>
         </div>
+
     );
 };
 
